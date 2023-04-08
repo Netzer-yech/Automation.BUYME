100cfb9e021c83dca236efebe820481f06dfd7a0
Netzer-yech <netzer.yech@gmail.com>
2023-04-08 23:18:26 +0300

    delete comments

--- pick_business_test.py
+++ pick_business_test.py
@@ -1,39 +1,5 @@
 from base_page import BasePage
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-
-# chrome_option = Options()
-# chrome_option.add_argument("--start-maximized")
-# driver = webdriver.Chrome(service=Service("C:\\automation course\\chromedriver.exe"), options=chrome_option)
-# driver.get('https://buyme.co.il/search?budget=3&category=16&region=11')
-# wait = WebDriverWait(driver, 10)
-# action = ActionChains(driver)
-# driver.implicitly_wait(5)
-
-
-# def test_pick_business():
-#     website_url = 'https://buyme.co.il/search?budget=3&category=16&region=11'
-#     wait.until(ec.url_to_be(website_url))
-#     assert driver.current_url == website_url
-#     cards = driver.find_elements(By.XPATH, value='//span[@class="name bm-subtitle-1"]')
-#     for card in cards:
-#         if card.text == "בית תאילנדי":
-#             card.click()
-#             break
-#
-#     driver.find_element(By.XPATH, value='//input[@placeholder="הכנס סכום"]').send_keys('250')
-#     driver.find_element(By.XPATH, value='//button[@type="submit"]').submit()
-
-
-
-
-# page need to write as POM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 class Constants():
 
     LOCATOR = By.XPATH
