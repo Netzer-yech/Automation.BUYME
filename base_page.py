a5331d48a1086591c0ba91b028a20206abba4f61
Netzer-yech <netzer.yech@gmail.com>
2023-04-13 21:51:02 +0300

    comments made in main and base-page

--- base_page.py
+++ base_page.py
@@ -13,52 +13,55 @@ logging.basicConfig(
                     datefmt='%H:%M:%S',
                     level=logging.ERROR,
                     force='True')
+# configuration of the logger and the log file
 
 class BasePage():
     def __init__(self, driver):
-        self.driver = driver
+        self.driver = driver     # construct the driver
 
-    def click_element(self, locator_type, locator_value):
-        try:
+    def click_element(self, locator_type, locator_value):   # click function billed-in find element function
+        try:                                                # using constant arguments
             self.driver.find_element(locator_type, value=locator_value).click()
         except Exception as exception:
-            logger.error(str(exception))
+            logger.error(str(exception))                    # catch the exceptions, append to logger
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+                                                            # take screenshot of exception and put in allure report
 
-    def find_textbox_and_clear(self, locator_type, locator_value):
+    def find_textbox_and_clear(self, locator_type, locator_value):      # clearing a text box element
         try:
             self.driver.find_element(locator_type, value=locator_value).clear()
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-    def wait_and_click(self, locator):
-        try:
+
+    def wait_and_click(self, locator):                   # wait and click function billed-in find element function
+        try:                                             # using WebDriverWait to wait(10sec) for presence of element
             wait(self.driver, 10).until(ec.presence_of_element_located(locator))
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def wait_and_return_text(self, locator):
+    def wait_and_return_text(self, locator):                   # wait and return text of an element function
         try:
             element = wait(self.driver, 10).until(ec.presence_of_element_located(locator))
             return element.text
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-    def send_text(self, locator_type, locator_value, text):
+    def send_text(self, locator_type, locator_value, text):     # send text function billed-in find element function
         try:
             self.driver.find_element(locator_type, value=locator_value).send_keys(text)
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def find_elm_and_submit(self, locator_type, locator_value):
+    def find_elm_and_submit(self, locator_type, locator_value):     # find element and submit
         try:
             self.find_web_elm(locator_type, locator_value).submit()
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-    def find_web_elm(self, locator_type, locator_value):
+    def find_web_elm(self, locator_type, locator_value):            # find element
         try:
             element = self.driver.find_element(locator_type, value=locator_value)
             return element
@@ -67,7 +70,7 @@ class BasePage():
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
 
-    def find_and_return_web_elm_text(self, locator_type, locator_value):
+    def find_and_return_web_elm_text(self, locator_type, locator_value):   # find element and return text
         try:
             element = self.driver.find_element(locator_type, value=locator_value)
             return element.text
@@ -75,7 +78,7 @@ class BasePage():
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def find_elements(self, locator_type, locator_value):
+    def find_elements(self, locator_type, locator_value):        # find elements as a list
         try:
             return self.driver.find_elements(locator_type, value=locator_value)
         except Exception as exception:
@@ -89,31 +92,32 @@ class BasePage():
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-
-    def get_current_url(self):
+    #     find an element using relative locator
+    def get_current_url(self):              # getting the current url
         try:
             return self.driver.current_url
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def wait_for_url(self, url):
+    def wait_for_url(self, url):            # waiting for current url to be as expected
         try:
             wait(self.driver, 10).until(ec.url_to_be(url))
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def go_to_url(self, url):
+    def go_to_url(self, url):              # go to specific url
         try:
             self.driver.get(url)
         except Exception as exception:
             logger.error(str(exception))
             allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def scroll_down_and_take_screenshot(self):
+    def scroll_down_and_take_screenshot(self):      # scroll down in the website using java-script executor
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
-    def go_to_last_page(self):
+    def go_to_last_page(self):                      # go back to last website page
         self.driver.back()
+
--- main_test.py
+++ main_test.py
@@ -8,13 +8,13 @@ from home_screen_test import HomeScreen
 from pick_business_test import PickBusiness
 from sender_and_receiver import SenderReceiver
 from extras_test import Extras
-
-class TestBuyMeWebsite(TestCase):
-    def setUp(self) -> None:
+# Import of relevant modules
+class TestBuyMeWebsite(TestCase):                  # using TestCase to organize the tests
+    def setUp(self) -> None:                       #  Set up any necessary resources or configurations for the test
         self.chrome_option = Options()
-        self.chrome_option.add_argument("--start-maximized")
-        json_file = open('config.json', 'r')
-        data = json.load(json_file)
+        self.chrome_option.add_argument("--start-maximized")  # using Options class for "start maximized" the browser
+        json_file = open('config.json', 'r')       # open the json file
+        data = json.load(json_file)                # loading the json content to the variable "data"
         browser = data['browserType']
         url = data['url']
         if browser == 'chrome' and url == "https://buyme.co.il/":
@@ -25,24 +25,24 @@ class TestBuyMeWebsite(TestCase):
         elif browser == 'edge' and url == "https://buyme.co.il/":
             self.driver = webdriver.Edge(service=Service("C:\\automation course\\msedgedriver.exe"))
             self.driver.get('https://buyme.co.il/')
-            json_file.close()
-        self.driver.implicitly_wait(10)
+            json_file.close()  # using if statement for reading the json content and define the driver and url for the program
+        self.driver.implicitly_wait(5)                # waiting generally for element to appear, for safety
         self.registration = Registration(self.driver)
         self.home_screen = HomeScreen(self.driver)
         self.pick_business = PickBusiness(self.driver)
         self.sender_receiver = SenderReceiver(self.driver)
-        self.extras = Extras(self.driver)
-    # def test_1_registration(self):
-    #     self.registration.test_registration()
-    # def test_2_home_screen(self):
-    #     self.home_screen.test_home_screen()
-    # def test_3_pick_business(self):
-    #     self.pick_business.test_pick_business()
-    # def test_4_sender_receiver(self):
-    #     self.sender_receiver.test_sender_receiver()
+        self.extras = Extras(self.driver)            # define the objects of the different test using POM
+    def test_1_registration(self):
+        self.registration.test_registration()
+    def test_2_home_screen(self):
+        self.home_screen.test_home_screen()
+    def test_3_pick_business(self):                  # each function calling the test from his module
+        self.pick_business.test_pick_business()
+    def test_4_sender_receiver(self):
+        self.sender_receiver.test_sender_receiver()
     def test_5_extras(self):
         self.extras.test_home_screen_error()
         self.extras.test_screenshot_buttom_of_page()
     def tearDown(self) -> None:
-        self.driver.quit()
+        self.driver.quit()                           # tearDown for ending the main test
 
