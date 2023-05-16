237641ac822cf1930aa80d3906befeaf51ccd5a8
Netzer-yech <netzer.yech@gmail.com>
2023-05-16 17:13:13 +0300

    without cloud testing

--- main_test.py
+++ main_test.py
@@ -10,28 +10,23 @@ from sender_and_receiver import SenderReceiver
 from extras_test import Extras
 # Import of relevant modules
 class TestBuyMeWebsite(TestCase):                  # using TestCase to organize the tests
-    def setUp(self) -> None:
-                                                    #Set up cloud testing option
-        options = Options()
-        options.browser_version = 'latest'
-        options.platform_name = 'Windows 11'
-        sauce_options = {'username': "oauth-netzer.yech-ac1d7",
-                         'browserName': 'Chrome',
-                         'build': 'Buy Me Website testing',
-                         'name': 'Cloud testing',
-                         'accessKey': '066cb6c7-0239-4c78-b992-4d3af5ea4b7e',
-                         'version': '105'}
-
-        options.set_capability('sauce:options', sauce_options)
-        sauce_url = "https://oauth-netzer.yech-ac1d7:066cb6c7-0239-4c78-b992-4d3af5ea4b7e@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
-        self.driver = webdriver.Remote(command_executor=sauce_url, options=options)
+    def setUp(self) -> None:                       #  Set up any necessary resources or configurations for the test
+        self.chrome_option = Options()
+        self.chrome_option.add_argument("--start-maximized")  # using Options class for "start maximized" the browser
         json_file = open('config.json', 'r')       # open the json file
         data = json.load(json_file)                # loading the json content to the variable "data"
+        browser = data['browserType']
         url = data['url']
-        if url == "https://buyme.co.il/":
-            self.driver.get(url)
-            json_file.close()                      # reading the json content and define the driver and url for the program
-        self.driver.implicitly_wait(3)              # waiting generally for element to appear, for safety
+        if browser == 'chrome' and url == "https://buyme.co.il/":
+            self.driver = webdriver.Chrome(service=Service(
+                "C:\\automation course\\chromedriver.exe"), options=self.chrome_option)
+            self.driver.get('https://buyme.co.il/')
+            json_file.close()
+        elif browser == 'edge' and url == "https://buyme.co.il/":
+            self.driver = webdriver.Edge(service=Service("C:\\automation course\\msedgedriver.exe"))
+            self.driver.get('https://buyme.co.il/')
+            json_file.close()                       # reading the json content and define the driver and url for the program
+        self.driver.implicitly_wait(5)              # waiting generally for element to appear, for safety
         self.registration = Registration(self.driver)
         self.home_screen = HomeScreen(self.driver)
         self.pick_business = PickBusiness(self.driver)
@@ -50,4 +45,3 @@ class TestBuyMeWebsite(TestCase):                  # using TestCase to organize 
         self.extras.test_screenshot_buttom_of_page()
     def tearDown(self) -> None:
         self.driver.quit()                           # tearDown for ending the main test
-
